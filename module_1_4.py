77b163547d2e880a4880fe4fe2fcedff74773c72
kapitosha <lunevalubasa@gmail.com>
2024-12-03 14:43:42 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('Введите любой текст: ')
+print('Количество символов в вашем тексте:', len(my_string))
+print('Верхний регистр:', my_string.upper())
+print('Нижний регистр:', my_string.lower())
+print('Без пробелов:', my_string.replace(' ',''))
+print('Первый символ:', my_string[0])
+print('Последний символ:', my_string[-1])
